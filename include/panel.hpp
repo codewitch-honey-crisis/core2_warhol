c84274f0d49ee35f288ce15f7be9b0c91f37c962
codewitch-honey-crisis <gaztoast@gmail.com>
2024-06-18 18:15:32 -0700

    transfer buffer size

--- include/panel.hpp
+++ include/panel.hpp
@@ -3,7 +3,7 @@
 #include <stddef.h>
 #include "ui.hpp"
 // use two 32KB buffers (DMA)
-constexpr const size_t panel_transfer_buffer_size = 32*1024;
+constexpr const size_t panel_transfer_buffer_size = 320*60*2;
 extern uint8_t* panel_transfer_buffer1;
 extern uint8_t* panel_transfer_buffer2;
 
